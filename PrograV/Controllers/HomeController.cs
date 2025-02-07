fd2a184b22bef06735caeb2b5654139151122700
JTS628 <jj628@hotmail.com>
2025-02-06 18:39:53 -0600

    Add project files.

---
+++ PrograV/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Firebase.Auth;
+using Firebase.Auth.Providers;
+using Microsoft.AspNetCore.Mvc;
+using PrograV.Firebase;
+using PrograV.Models;
+
+namespace PrograV.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        
+       
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ PrograV/Firebase/FirebaseAuthHelper.cs
@@ -0,0 +1,27 @@
+﻿using Firebase.Auth;
+using Firebase.Auth.Providers;
+
+namespace PrograV.Firebase
+{
+    public static class FirebaseAuthHelper
+    {
+        public const string firebaseAppId = "";
+        public const string firebaseApiKey = "";
+
+        public static FirebaseAuthClient setFirebaseAuthClient()
+        {
+            var auth = new FirebaseAuthClient(new FirebaseAuthConfig()
+            {
+                ApiKey = firebaseApiKey,
+                AuthDomain = firebaseAppId,
+                Providers = new FirebaseAuthProvider[]
+            {
+            new EmailProvider()
+            }
+            });
+
+            return auth;
+        }
+
+    }
+}
