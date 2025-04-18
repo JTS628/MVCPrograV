e0f3b18d8ee0f5d097ccc659024ab9362e368ad2
JTS628 <jj628@hotmail.com>
2025-04-18 14:51:49 -0600

    remove config data

--- PrograV/Firebase/FirebaseAuthHelper.cs
+++ PrograV/Firebase/FirebaseAuthHelper.cs
@@ -5,8 +5,8 @@ namespace PrograV.Firebase
 {
     public static class FirebaseAuthHelper
     {
-        public const string firebaseAppId = "prograv-3a89e";
-        public const string firebaseApiKey = "AIzaSyA0XnB5TU7qM6JyE-YD7K-ib5gjBhRxN2I";
+        public const string firebaseAppId = "";
+        public const string firebaseApiKey = "";
 
         public static FirebaseAuthClient setFirebaseAuthClient()
         {
